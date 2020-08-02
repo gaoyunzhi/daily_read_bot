2371de40ed062f13aba2f7dcc6980cf317897ec8
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 13:38:39 -0700

    commit

---
+++ daily_read_bot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import webgram
+from telegram_util import log_on_fail
+from telegram.ext import Updater
+
+with open('token') as f:
+    token = f.read().strip()
+
+tele = Updater(token, use_context=True)
+debug_group = bot.get_chat(420074357)
+
+with open('pool') as f:
+    pool = [channel.strip() for channel in f.readlines()]
+pool = [channel for channel in pool if channel]
+
+SevenDay = 7 * 24 * 60 * 60
+
+def getPosts():
+    result = []
+    for channel in pool:
+        posts = webgram.get(channel)[1:]
+        result += posts
+        while posts and posts[0].time > time.time() - SevenDay:
+            posts = webgram.get(channel, posts[0].post_id, direction='before')
+            result += posts
+    print(len(result))
+    return result
+
+def getDailyRead():
+    posts = getPosts()
+
+
+def sendDailyRead(msg):
+    removeOldFiles('tmp', day=0.1)
+    if 'force' in msg.text:
+        removeOldFiles('tmp', day=0)
+    tele.bot.send_message(msg.chat_id, getDailyRead(), disable_web_page_preview=True)
+
+@log_on_fail(debug_group)
+def handleCommand(update, context):
+    msg = update.effective_message
+    if not msg.startswith('/dr'):
+        return
+    sendDailyRead(msg)
+    msg.delete()
+
+@log_on_fail(debug_group)
+def handlePrivate(update, context):
+    msg = update.effective_message
+    if not msg:
+        return
+    sendDailyRead(msg)
+
+if __name__ == '__main__':
+    dp = tele.dispatcher
+    dp.add_handler(MessageHandler(Filters.command & (^Filters.private), handleCommand))
+    dp.add_handler(MessageHandler(Filters.private, handlePrivate))
