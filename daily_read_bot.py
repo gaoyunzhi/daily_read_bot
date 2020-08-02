78d1a8bec91c5e0e0c211cc48716f1405a19c3f1
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-08-02 13:44:29 -0700

    commit

--- daily_read_bot.py
+++ daily_read_bot.py
@@ -3,13 +3,14 @@
 
 import webgram
 from telegram_util import log_on_fail
-from telegram.ext import Updater
+from telegram.ext import Updater, MessageHandler, Filters
+import time
 
 with open('token') as f:
     token = f.read().strip()
 
 tele = Updater(token, use_context=True)
-debug_group = bot.get_chat(420074357)
+debug_group = tele.bot.get_chat(420074357)
 
 with open('pool') as f:
     pool = [channel.strip() for channel in f.readlines()]
@@ -20,16 +21,19 @@ SevenDay = 7 * 24 * 60 * 60
 def getPosts():
     result = []
     for channel in pool:
-        posts = webgram.get(channel)[1:]
+        posts = webgram.getPosts(channel)[1:]
         result += posts
         while posts and posts[0].time > time.time() - SevenDay:
-            posts = webgram.get(channel, posts[0].post_id, direction='before')
+            posts = webgram.getPosts(channel, posts[0].post_id, direction='before')
             result += posts
     print(len(result))
     return result
 
 def getDailyRead():
     posts = getPosts()
+    posts = [post for post in posts in post.link]
+    [print(post.link) for post in posts]
+
 
 
 def sendDailyRead(msg):
@@ -54,6 +58,7 @@ def handlePrivate(update, context):
     sendDailyRead(msg)
 
 if __name__ == '__main__':
+    getDailyRead() # test
     dp = tele.dispatcher
-    dp.add_handler(MessageHandler(Filters.command & (^Filters.private), handleCommand))
+    dp.add_handler(MessageHandler(Filters.command & (~ Filters.private), handleCommand))
     dp.add_handler(MessageHandler(Filters.private, handlePrivate))
