776b6352a4f2acb9ee93e89d44cbea8088ab2aae
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-11-24 11:56:53 -0800

    commit

--- book.py
+++ book.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import cached_url
 
-def getDouban(book_name):
+book_prefix = 'https://book.douban.com/subject/'
+
+def getBookRecommendation(book_name):
 	search_url = 'https://www.douban.com/search?cat=1001&q=' + book_name
 	soup = BeautifulSoup(cached_url.get(search_url), features='lxml')
 	book = soup.find('a', {'onclick': True})
@@ -9,11 +11,16 @@ def getDouban(book_name):
 		return '豆瓣搜索失败'
 	douban_book_name = book.get('title')
 	sid = book.get('onclick').split('sid: ')[1].split(',')[0]
-	book_url = 'https://book.douban.com/subject/' + sid
+	book_url = book_prefix + sid
 	soup = BeautifulSoup(cached_url.get(book_url), features='lxml')
-	return '豆瓣书名： %s' + douban_book_name
-
-def getBookRecommendation(book_name):
-	return getDouban(book_name)
+	rating = soup.find('strong', class_='rating_num').text
+	related = []
+	for item in soup.find_all('a', {'href': True}):
+		if 'book-rec-books' not in str(item):
+			continue
+		if item.text.strip() and item['href'].startswith(book_prefix):
+			related.append('<a href="%s">%s</a>' % (item['href'], item.text.strip()))
+	return '豆瓣书名： %s\n豆瓣评分： %s\n\n相关书籍：\n%s' % (
+		douban_book_name, rating, '\n'.join(related))
 
 
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from book import getBookRecommendation
 
 def test():
-	getBookRecommendation('性心情')
+	print(getBookRecommendation('性心情'))
 
 if __name__ == '__main__':
 	test()
